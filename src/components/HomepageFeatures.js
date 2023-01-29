4951d075daf6bfe061250efdd04d6c26a22253aa
孙业盛 <1481633100@qq.com>
2023-01-29 18:34:38 +0800

    init

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -1,68 +1,68 @@
-import React from "react";
-import { useSpring, animated, useTrail } from "react-spring";
-import styled from "@emotion/styled";
-import BgImage from "@site/static/img/home/bg2.svg";
-import MusicUrl from "@site/static/img/home/music.svg";
-import Styles from "./HomepageFeatures.module.scss";
-import BillBill from "@site/static/img/home/billbill.svg";
-import QQ from "@site/static/img/home/qq.svg";
-import WeChat from "@site/static/img/home/weChat.svg";
+import React from 'react'
+import { useSpring, animated, useTrail } from 'react-spring'
+import styled from '@emotion/styled'
+import BgImage from '@site/static/img/home/bg2.svg'
+import MusicUrl from '@site/static/img/home/music.svg'
+import Styles from './HomepageFeatures.module.scss'
+import BillBill from '@site/static/img/home/billbill.svg'
+import QQ from '@site/static/img/home/qq.svg'
+import WeChat from '@site/static/img/home/weChat.svg'
 
 export default function HomepageFeatures() {
   const props = useSpring({
     opacity: 1,
-    transform: "translateY(0)",
-    from: { opacity: 0, transform: "translateY(3em)" },
+    transform: 'translateY(0)',
+    from: { opacity: 0, transform: 'translateY(3em)' },
     delay: 0,
-  });
+  })
 
   const props2 = useSpring({
     opacity: 1,
-    transform: "translateY(0)",
-    from: { opacity: 0, transform: "translateY(3em)" },
+    transform: 'translateY(0)',
+    from: { opacity: 0, transform: 'translateY(3em)' },
     delay: 300,
-  });
+  })
 
   const props3 = useSpring({
     opacity: 1,
-    transform: "translateY(0)",
-    from: { opacity: 0, transform: "translateY(3em)" },
+    transform: 'translateY(0)',
+    from: { opacity: 0, transform: 'translateY(3em)' },
     delay: 600,
-  });
+  })
 
   const animatedTexts = useTrail(5, {
-    from: { opacity: 0, transform: "translateY(3em)" },
-    to: { opacity: 1, transform: "translateY(0)" },
+    from: { opacity: 0, transform: 'translateY(3em)' },
+    to: { opacity: 1, transform: 'translateY(0)' },
     config: {
       mass: 3,
       friction: 45,
       tension: 460,
     },
-  });
+  })
 
   return (
     <div className={Styles.mainWrapper}>
       <div style={props} className={Styles.title}>
         <div>
           Welcome to
-          <span>JiuRan</span>
+          <span>Sunyesheng</span>
           's Channel
         </div>
-        <div class={Styles.subTitle}>计算机编程爱好者, 深耕前端</div>
+        <div class={Styles.subTitle}>计算机编程爱好者</div>
         {/* <SocialLinks animatedProps={animatedTexts[4]} /> */}
       </div>
       <div
         style={{
-          display: "flex",
-          alignItems: "end",
-          height: "calc(100vh - 60px)",
+          display: 'flex',
+          alignItems: 'end',
+          height: 'calc(100vh - 60px)',
         }}
       >
         <SvgBackGround />
       </div>
       {/* <Music></Music> */}
     </div>
-  );
+  )
 }
 
 function SocialLinks({ animatedProps, ...props }) {
@@ -79,7 +79,7 @@ function SocialLinks({ animatedProps, ...props }) {
         <WeChat />
       </a>
     </animated.div>
-  );
+  )
 }
 
 const SvgBackGround = styled(BgImage)`
@@ -89,10 +89,10 @@ const SvgBackGround = styled(BgImage)`
   background-position: right center;
   background-size: cover;
   margin-bottom: 20px;
-`;
+`
 
 const Music = styled(MusicUrl)`
   position: fixed;
   bottom: 0px;
   right: 0px;
-`;
+`
