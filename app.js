6a02f633b0a7f994df0b8742dd8903b64bc7e4fc
孙业盛 <1481633100@qq.com>
2023-02-12 20:45:38 +0800

    feat: http 跳 https

--- app.js
+++ app.js
@@ -2,16 +2,27 @@ const express = require('express')
 const https = require('https')
 const fs = require('fs')
 
-const app = express()
-
-app.use(express.static('./build'))
-
 const options = {
   key: fs.readFileSync('./https/ngix/9278507_sunyesheng.cn.key.crt'),
   cert: fs.readFileSync('./https/ngix/9278507_sunyesheng.cn.crt'),
 }
 
-const server = https.createServer(options, app)
+const httpApp = express()
+const httpsApp = express()
+
+httpsApp.use(express.static('./build'))
+
+const server = https.createServer(options, httpsApp)
+
+httpApp.get('*', (req, res, next) => {
+  let host = req.headers.host
+  host = host.replace(/\:\d+$/, '') // Remove port number
+  res.redirect(`https://${host}${req.path}`)
+})
+
+httpApp.listen(80, () => {
+  console.log('https://127.0.0.1')
+})
 
 server.listen(443, () => {
   console.log('https://127.0.0.1:443')
