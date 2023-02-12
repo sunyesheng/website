86284ce5d8a4a2bbb5577badfa5cfb52daf6c379
孙业盛 <1481633100@qq.com>
2023-02-12 18:02:46 +0800

    feat: add https

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const express = require('express')
-const https = require('https')
-const fs = require('fs')
+// const https = require('https')
+// const fs = require('fs')
 
 const app = express()
 
-const options = {
-  pfx: fs.readFileSync('./https/9278507_sunyesheng.cn.pfx'),
-  passphrase: '50wtktr9',
-}
+// const options = {
+//   pfx: fs.readFileSync('./https/9278507_sunyesheng.cn.pfx'),
+//   passphrase: '50wtktr9',
+// }
 
-const server = https.createServer(options, app)
+// const server = https.createServer(options, app)
 
 app.use(express.static('./build'))
 
-server.listen(80, () => {
+app.listen(80, () => {
   console.log('http://127.0.0.1')
 })
