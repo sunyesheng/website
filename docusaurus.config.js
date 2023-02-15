b5cb7b121c71a0f68d3bf83c29741da177684242
孙业盛 <1481633100@qq.com>
2023-02-15 23:15:37 +0800

    feat: algolia增加搜索功能

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,33 +13,10 @@ module.exports = {
   themeConfig: {
     // 增加C++语言高亮提示，其他语言参考:  https://prismjs.com/#supported-languages
     algolia: {
-      // The application ID provided by Algolia
       appId: 'GD9V1PD0YO',
-
-      // Public API key: it is safe to commit it
       apiKey: '9ed53a6aca260d7c24fec5cec79d4584',
-
       indexName: 'sunyesheng',
-
-      // Optional: see doc section below
-      // contextualSearch: true,
-
-      // // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
-      // externalUrlRegex: 'external\\.com|domain\\.com',
-
-      // // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
-      // replaceSearchResultPathname: {
-      //   from: '/docs/', // or as RegExp: /\/docs\//
-      //   to: '/',
-      // },
-
-      // // Optional: Algolia search parameters
-      // searchParameters: {},
-
-      // // Optional: path for search page that enabled by default (`false` to disable it)
-      // searchPagePath: 'search',
-
-      //... other Algolia params
+      contextualSearch: false,
     },
     prism: {
       additionalLanguages: ['cpp'],
